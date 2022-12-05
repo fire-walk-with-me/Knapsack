56fe4476c557973970ffed76569416d141f13b06
fire-walk-with-me <mrc.e.bra@gmail.com>
2022-12-05 10:55:25 +0100

    created Mono-game file

---
+++ Knapsack/Knapsack/Game1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+namespace BallProject
+{
+   
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";  
+        }
+
+        protected override void Initialize()
+        {
+           base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+        }
+
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+
+
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
